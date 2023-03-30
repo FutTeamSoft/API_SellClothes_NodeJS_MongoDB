42e4472e1afd256fb26a315b4748556540d1a333
Le Dinh Phuc <38375962+dinhphuc1309@users.noreply.github.com>
2023-03-30 11:19:07 +0700

    xong màn hình home

---
+++ frontend/src/components/Product/index.jsx
@@ -0,0 +1,52 @@
+import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import classNames from "classnames/bind";
+
+import styles from "./Product.module.scss";
+import config from "~/config";
+
+const cx = classNames.bind(styles);
+
+function Product({ product, className, ...passProps }) {
+  let VND = new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  });
+
+  const classes = cx("product-wrap", {
+    [className]: className,
+  });
+
+  return (
+    <Col md={3} className={classes} {...passProps}>
+      <div className={cx("product-inner")}>
+        <Link
+          to={
+            config.routes.products +
+            "/" +
+            product.NameProduct.replace(/\s+/g, "-").toLowerCase()
+          }
+          state={product}
+        >
+          <img
+            className={cx("product-image")}
+            src={product.ImageProduct.DuongDanHinh}
+            alt={product.NameProduct}
+          />
+
+          <div className={cx("product-info-wrap")}>
+            <p title={product.NameProduct} className={cx("product-name")}>
+              {product.NameProduct}
+            </p>
+
+            <p className={cx("product-price")}>
+              {VND.format(product.PriceProduct)}
+            </p>
+          </div>
+        </Link>
+      </div>
+    </Col>
+  );
+}
+
+export default Product;
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import classNames from "classnames/bind";
+import { Row } from "react-bootstrap";
 
 import Button from "~/components/Button";
-
+import Product from "~/components/Product";
 import config from "~/config";
 import style from "./Home.module.scss";
+import * as productServices from "~/services/productServices";
+import { useChangeStateNav } from "~/hooks";
 
 const cx = classNames.bind(style);
 
 function Home() {
+  const location = useLocation();
+  const [newProduct, setNewProduct] = useState([]);
+
+  useChangeStateNav(location);
+
+  useEffect(() => {
+    const fetchNewProducts = async () => {
+      const result = await productServices.newProduct(4);
+      setNewProduct(result);
+    };
+
+    fetchNewProducts();
+  }, []);
+
   return (
     <>
       <div className={cx("img-wrap")}>
@@ -15,10 +34,24 @@ function Home() {
         <div className={cx("caption")}>
           <h1>{config.texts.welcome}</h1>
           <p>{config.texts.describeHome}</p>
-          <Button primary to={config.routes.product}>
+          <Button className={cx("button")} primary to={config.routes.products}>
             {config.texts.shopNow}
           </Button>
         </div>
+      </div>
+      <div className={cx("new-product-wrap")}>
+        <h2 className={cx("new-product-wrap-tile")}>
+          {config.texts.titleNewProducts}
+        </h2>
+        <Row
+          className={`${cx(
+            "new-product-wrap-content"
+          )} justify-content-md-center`}
+        >
+          {newProduct?.map((result, index) => (
+            <Product xxl={2} key={index} product={result} />
+          ))}
+        </Row>
       </div>
     </>
   );
